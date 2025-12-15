93ca841aa56632933ff3488ab75b498ac3f4e93a
JIA-MIN_CHEN <41343133@nfu.edu.tw>
2025-12-15 14:56:05 +0800

    fin1

--- img.cpp
+++ img.cpp
@@ -1,7 +1,77 @@
 #include "img.h"
+#include <QHBoxLayout>
+#include <QMenuBar>
+#include <QFileDialog>
+#include <QDebug>
 
 IMG::IMG(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    setWindowTitle (QStringLiteral("影像處理"));
+    central =new QWidget();
+    QHBoxLayout *mainLayout = new QHBoxLayout (central);
+        imgWin = new QLabel();
+    QPixmap *initPixmap = new QPixmap(300,200);
+    initPixmap->fill (QColor (255, 255, 255));
+    imgWin->resize (300,200);
+    imgWin->setScaledContents(true);
+    imgWin->setPixmap (*initPixmap);
+    mainLayout->addWidget (imgWin);
+    setCentralWidget (central);
+    createActions();
+    createMenus();
+    createToolBars();
+}
 
 IMG::~IMG() {}
+void IMG::createActions ()
+{
+    openFileAction = new QAction (QStringLiteral("開啟檔案&O"),this);
+    openFileAction->setShortcut (tr("Ctrl+0"));
+    openFileAction->setStatusTip (QStringLiteral("開啟影像檔案"));
+    connect (openFileAction, SIGNAL (triggered()), this, SLOT (showOpenFile()));
+    exitAction = new QAction (QStringLiteral("結束&Q"),this);
+    exitAction->setShortcut (tr("Ctrl+Q"));
+    exitAction->setStatusTip (QStringLiteral("退出程式"));
+    connect (exitAction, SIGNAL(triggered()), this, SLOT (close()));
+}
+void IMG::createMenus()
+{
+    fileMenu =menuBar ()->addMenu (QStringLiteral("檔案&F"));
+    fileMenu->addAction (openFileAction);
+    fileMenu->addAction(exitAction);
+}
+void IMG::createToolBars ()
+{
+    fileTool =addToolBar("file");
+    fileTool->addAction (openFileAction);
+}
+void IMG::loadFile(QString filename)
+{
+    qDebug() <<QString("file name: %1").arg(filename);
+    QByteArray ba=filename.toLatin1();
+    printf("FN:%s\n", (char *) ba.data());
+    img.load(filename);
+    imgWin->setPixmap (QPixmap:: fromImage(img));
+}
+void IMG::showOpenFile()
+{
+    filename= QFileDialog::getOpenFileName (this,
+                                            QStringLiteral("開啟影像"),
+                                            tr("."),
+                                            "bmp(*.bmp);;png(*.png)"
+                                            ";;Jpeg(*.jpg)");
+    if (!filename.isEmpty())
+    {
+        if (img.isNull())
+        {
+            loadFile(filename);
+        }
+        else
+        {
+            IMG *newIPWin = new IMG();
+            newIPWin->show();
+            newIPWin->loadFile(filename);
+        }
+    }
+}
--- img.h
+++ img.h
@@ -2,7 +2,11 @@
 #define IMG_H
 
 #include <QMainWindow>
-
+#include <QAction>
+#include <QMenu>
+#include <QToolBar>
+#include <QImage>
+#include <QLabel>
 class IMG : public QMainWindow
 {
     Q_OBJECT
@@ -10,5 +14,22 @@ class IMG : public QMainWindow
 public:
     IMG(QWidget *parent = nullptr);
     ~IMG();
+    void createActions();
+    void createMenus();
+    void createToolBars();
+    void loadFile (QString filename);
+private slots:
+    void showOpenFile();
+
+private:
+    QWidget *central;
+    QMenu *fileMenu;
+    QToolBar *fileTool;
+    QImage img;
+    QString filename;
+    QLabel *imgWin;
+    QAction *openFileAction;
+    QAction *exitAction;
+
 };
 #endif // IMG_H
